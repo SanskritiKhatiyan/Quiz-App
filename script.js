a0085caac7d0b7e6426f2606a12efe544a163df8
Sanskriti Khatiyan <khatiyan.sanskriti@gmail.com>
2021-05-28 20:31:14 +0530

    on way to complete js file

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
 const info_box= document.querySelector(".info_box");
 const continue_btn= info_box.querySelector(".buttons .restart");
 const quit_btn= info_box.querySelector(".buttons .quit");
+const body = document.querySelector('body');
+const quiz_box= document.querySelector(".quiz_box");
 
 quit_btn.onclick=()=>{
     window.history.go(-1);
 }
 
-// continue_btn.addEventListener('click',()=>{
-    
-//    document.body.style.backgroundImage = "url('download.jpg')";
-//     body.classList.remove('animation')
-   
-// })
-
-const button = document.querySelector('button.restart')
-const body = document.querySelector('body')
-button.addEventListener('click',()=>{
-    body.classList.remove('abc')
-    body.classList.add('bg')
-})
+continue_btn.onclick=()=>{
+    body.classList.remove('abc');
+    body.classList.add('bg');
+    quiz_box.classList.add('activeQuiz');
+    info_box.classList.add('activeInfo');
+}
 
 
 
