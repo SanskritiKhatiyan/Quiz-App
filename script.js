3e7201d34e003ae90f25c5ca93c25eeb258abda1
Sanskriti Khatiyan <khatiyan.sanskriti@gmail.com>
2021-05-29 18:24:41 +0530

    on the way

--- script.js
+++ script.js
@@ -11,128 +11,11 @@ const time_line=document.querySelector(".time_line");
 
 let question_number=0;
 let question_count=1;
+let timer;
+let liner;
+let index;
+let userScore=0;
 
-let questions = [
-    {
-    number: 1,
-    question: "What does xyz means",
-    answer: "xyz",
-    options: [
-      "abc",
-      "def",
-      "xyz",
-      "xyq"
-    ]
-  },
-    
-  {
-    number: 2,
-    question: "What does xyz means",
-    answer: "xyz",
-    options: [
-      "abc",
-      "def",
-      "xyz",
-      "xyq"
-    ]
-  },
-
-  {
-    number: 3,
-    question: "What does xyz means",
-    answer: "xyz",
-    options: [
-      "abc",
-      "def",
-      "xyz",
-      "xyq"
-    ]
-  },
-
-  {
-    number: 4,
-    question: "What does xyz means",
-    answer: "xyz",
-    options: [
-      "abc",
-      "def",
-      "xyz",
-      "xyq"
-    ]
-  },
-
-  {
-    number: 5,
-    question: "What does xyz means",
-    answer: "xyz",
-    options: [
-      "abc",
-      "def",
-      "xyz",
-      "xyq"
-    ]
-  },
-
-  {
-    number: 6,
-    question: "What does xyz means",
-    answer: "xyz",
-    options: [
-      "abc",
-      "def",
-      "xyz",
-      "xyq"
-    ]
-  },
-
-  {
-    number: 7,
-    question: "What does xyz means",
-    answer: "xyz",
-    options: [
-      "abc",
-      "def",
-      "xyz",
-      "xyq"
-    ]
-  },
-
-  {
-    number: 8,
-    question: "What does xyz means",
-    answer: "xyz",
-    options: [
-      "abc",
-      "def",
-      "xyz",
-      "xyq"
-    ]
-  },
-
-  {
-    number: 9,
-    question: "What does xyz means",
-    answer: "xyz",
-    options: [
-      "abc",
-      "def",
-      "xyz",
-      "xyq"
-    ]
-  },
-
-  {
-    number: 10,
-    question: "What does xyz means",
-    answer: "xyz",
-    options: [
-      "abc",
-      "def",
-      "xyz",
-      "xyq"
-    ]
-  },
-];
 
 quit_btn.onclick=()=>{
     window.history.go(-1);
@@ -146,6 +29,7 @@ continue_btn.onclick=()=>{
     showquestions(question_number);
     question_counter(question_count);
     start_time(15);
+    start_timerline(0);
 
 }
 
@@ -153,11 +37,36 @@ function showquestions(index){
     const que_text= document.querySelector(".que_text");
     const option_list=document.querySelector(".option_list");
     let que_template= '<span>'+ questions[index].number+ "." + questions[index].question + '</span>';
-    let option_template= '<div>'+ questions[index].options[0]+'</div>'+ '<div>'+ questions[index].options[1] +'</div>'+
-    '<div>'+  questions[index].options[2]+'</div>'+'<div>'+  questions[index].options[3]+'</div>';
+    let option_template= '<div class="option">'+ questions[index].options[0]+'</div>'+ '<div class="option">'+ questions[index].options[1] +'</div>'+
+    '<div class="option">'+  questions[index].options[2]+'</div>'+'<div class="option">'+  questions[index].options[3]+'</div>';
     que_text.innerHTML=que_template;
     option_list.innerHTML=option_template;
+
+    const option= option_list.querySelector(".option");
+   
+    for(i=0;i<option.length;i++){
+      option[i].setAttribute("onclick","option_selected(this)");
+    }
+  
 } 
+
+function option_selected(choosed_answer){
+  clearInterval("liner");
+  clearInterval("timer");
+  let users_answser=choosed_answer.textContent;
+  let correct_answer=questions[question_number].answer;
+  const allOptions = option_list.children.length;
+
+  if(correct_answer==users_answser){
+    console.log("working");
+    userScore += 1;
+    option.classList.add("correct");
+  }
+
+  next_btn.classList.add(".show");
+}
+
+
 
 function question_counter(index){
     const total_que= document.querySelector("footer .total_que");
@@ -176,15 +85,27 @@ function start_time(index){
     }
 }
 
-next_btn.onclick=()=>{
-    if(question_number<=10){
-   showquestions(question_number++);
-   question_counter(question_count++);
-   clearInterval(timer);
-   start_time(15);
-
+function start_timerline(index){
+  liner=setInterval(timeline,65);
+  function timeline(){
+    time_line.style.width = index + "px";
+    index++;
+    if(index>250){
+      clearInterval(liner);
     }
-    else{
+  }
+}
+
+next_btn.onclick=()=>{
+  if(question_number<=10){
+    showquestions(question_number+=1);
+    question_counter(question_count+=1);
+    clearInterval(timer);
+    start_time(15);
+    clearInterval(liner);
+    start_timerline(0);
+  }
+  else{
 
     }
 }
