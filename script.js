69b2d533508dfe57d4c472efdbb01b59b5cdbfac
Sanskriti Khatiyan <khatiyan.sanskriti@gmail.com>
2021-06-02 15:04:03 +0530

    added emojis :)

--- script.js
+++ script.js
@@ -23,6 +23,8 @@ let liner;
 let index;
 let time_close;
 let userScore=0;
+let tickemoji= '<div><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="green" class="bi bi-emoji-sunglasses-fill" viewBox="0 0 16 16"><path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zM2.31 5.243A1 1 0 0 1 3.28 4H6a1 1 0 0 1 1 1v.116A4.22 4.22 0 0 1 8 5c.35 0 .69.04 1 .116V5a1 1 0 0 1 1-1h2.72a1 1 0 0 1 .97 1.243l-.311 1.242A2 2 0 0 1 11.439 8H11a2 2 0 0 1-1.994-1.839A2.99 2.99 0 0 0 8 6c-.393 0-.74.064-1.006.161A2 2 0 0 1 5 8h-.438a2 2 0 0 1-1.94-1.515L2.31 5.243zM4.969 9.75A3.498 3.498 0 0 0 8 11.5a3.498 3.498 0 0 0 3.032-1.75.5.5 0 1 1 .866.5A4.498 4.498 0 0 1 8 12.5a4.498 4.498 0 0 1-3.898-2.25.5.5 0 0 1 .866-.5z"/></svg></div>';
+let wrongemoji= '<div><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="red" class="bi bi-emoji-dizzy-fill" viewBox="0 0 16 16"><path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zM4.146 5.146a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 1 1 .708.708l-.647.646.647.646a.5.5 0 1 1-.708.708L5.5 7.207l-.646.647a.5.5 0 1 1-.708-.708l.647-.646-.647-.646a.5.5 0 0 1 0-.708zm5 0a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708.708l-.647.646.647.646a.5.5 0 0 1-.708.708l-.646-.647-.646.647a.5.5 0 1 1-.708-.708l.647-.646-.647-.646a.5.5 0 0 1 0-.708zM8 13a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"/></svg> </div>';
 
 
 quit_btn.onclick=()=>{
@@ -71,9 +73,11 @@ function option_selected(choosed_answer){
   if(users_answser == correct_answer){
     userScore+=1;
     choosed_answer.classList.add("correct");
+    choosed_answer.insertAdjacentHTML("beforeend", tickemoji);
   }
   else{
     choosed_answer.classList.add("incorrect");
+    choosed_answer.insertAdjacentHTML("beforeend", wrongemoji);
 
       for(i=0;i<option_list.children.length;i++){
         let auto_answer=option_list.children[i].textContent;
@@ -82,7 +86,7 @@ function option_selected(choosed_answer){
         }
       }
   }
-
+  console.log(question_count);
   for(i=0;i<option_list.children.length;i++){
       option_list.children[i].classList.add("disabled");
   }
