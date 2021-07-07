5f8e01e4c0d90271152e9634d42064a5bf891f35
Sanskriti Khatiyan <khatiyan.sanskriti@gmail.com>
2021-07-07 23:29:26 +0530

    on process

--- index.js
+++ index.js
@@ -1,19 +1,65 @@
-// REQUIRED
+// =======================REQUIRED===============================
 const express= require("express");
 const app= express();
 const User= require("./models/user");
+const mongoose= require("mongoose");
+const bcrypt= require("bcrypt");
+
+
+// ===================CONNECTING DATABASE========================
+mongoose.connect('mongodb://localhost:27017/User_Database', { useNewUrlParser: true, useUnifiedTopology: true })
+.then(()=>{
+    console.log("mongo connection is done:)");
+})
+.catch(err=>{
+    console.log("connection error!!!");
+    console.log(err);
+})
+
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
+
+app.use(express.urlencoded( { extended: true} ));
+
+
+
+// =====================ROUTES===================================
+
+app.get('/', (req,res)=>{
+    res.send('This is home page');
+})
+
+
+app.get('/landing', (req, res)=>{
+    res.render('landing');
+})
 
 app.get('/register', (req,res)=>{
     res.render('register');
 })
 
+app.post('/register', async (req, res)=>{
+    const { password, username}= req.body;
+    const hash_pw= await bcrypt.hash(password, 12);
+    res.send(hash_pw);
+    const user= new User({
+        password: hash_pw,
+        username
+      });
+    await user.save();
+    // res.redirect('/landing')
+
+})
+
+app.get('/login', (req,res)=>{
+    res.render('login');
+})
 
 
 
-// Listening App
+
+// =======================Listening App===========================
 app.listen(2000,()=>{
     console.log("listening on port 2000!!")
 })
